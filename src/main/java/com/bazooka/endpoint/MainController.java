618cc350adabe054fe95b3a3b7e115f62c22d9e6
Paulo Sérgio do Nascimento <pasergio@unicamp.br>
2019-08-26 17:13:55 -0300

    alteração na job

--- src/main/java/com/bazooka/endpoint/MainController.java
+++ src/main/java/com/bazooka/endpoint/MainController.java
@@ -1,11 +1,12 @@
 package com.bazooka.endpoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@RestController("/")
 public class MainController {
 
     @GetMapping
--- src/main/java/com/bazooka/job/heroku/JobAutoGet.java
+++ src/main/java/com/bazooka/job/heroku/JobAutoGet.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -22,7 +24,8 @@ public class JobAutoGet {
             URL url = new URL("https://estados-cidades.herokuapp.com");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            //log.info("Http Code {}", con.getResponseCode());
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
             //log.info("Job success!");
 
         } catch (MalformedURLException e) {
