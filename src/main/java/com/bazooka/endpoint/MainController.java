bdf98c25d162854f50760ed47cf1c25a4c27b6dd
Paulo Sérgio do Nascimento <pasergio@unicamp.br>
2019-08-26 17:16:37 -0300

    alteração do intervalo de solicitação

--- src/main/java/com/bazooka/endpoint/MainController.java
+++ src/main/java/com/bazooka/endpoint/MainController.java
@@ -1,12 +1,11 @@
 package com.bazooka.endpoint;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/")
 public class MainController {
 
     @GetMapping
